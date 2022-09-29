f82f6c8f2e557695da92b002b546e3e819c20911
AminASTU <irgaliev01@mail.ru>
2022-09-30 01:35:30 +0400

    Отображение ходов на доске+очередность+завершение

--- gameplay.py
+++ gameplay.py
@@ -1,5 +1,17 @@
 from board import FillStartBoard, PrintBoard, arrayWord
-import re
+
+# Счетчик для ходов (Черные начинают партию)
+def create_counter():
+    i = 0
+    def func():
+        nonlocal i
+        i += 1
+        return i
+    return func
+
+counter = create_counter()
+
+board = FillStartBoard()
 
 # Если нет пустых координат - игра закончена
 def isGameOver(game):
@@ -8,28 +20,41 @@ def isGameOver(game):
         for j in range(len(game[i])):
             if (game[i][j] == '*'):
                 flag = False
-            return flag
+    return flag
 
 # Запрос хода
 def Move():
-    print('Твой ход: ')
+    step = counter()
+    print(step)
+
+    if (step % 2 == 0):
+        print('Ход Белых: ')
+    else:
+        print('Ход Черных: ')
+
     xy = input()
-    coordinate = CoordinateRules(xy)
+    if(xy.lower() == 'exit'):
+        return False
+    else: 
+        coordinate = CoordinateRules(xy.lower())
+        # Получение готовых координат
+        x_coord = int()
+        y_coord = int()
+        if(coordinate != 0):
+            x_coord = int(coordinate[1])
+            y_coord = int(coordinate[0])
 
-    # Получение готовых координат
-    x_str = ''
-    y_numb = int()
-    if(coordinate != 0):
-        x_str = coordinate[0]
-        y_numb = int(coordinate[1])
-
+        if (step % 2 == 0):
+            FillBoard_White(x_coord, y_coord)
+        else:
+            FillBoard_Black(x_coord, y_coord)
+    
 # Проверка на вторую координату (буква)
 def CheckWordInCoord(check):
-    f_word = False
     for word in range(0, len(arrayWord) - 2):
         if(check == arrayWord[word]):
-            f_word = True
-    return f_word
+            return word + 1
+    return -1
 
 # Проверка на первую координату (число)
 def CheckNumbInCoord(check):
@@ -49,30 +74,30 @@ def CoordinateRules(xy):
         f_error = True
     x = xy[0]
     y = xy[1]
-    numb = int()
-    str = ''
+    x_coord = int()
+    y_coord = int()
     # Формирование координаты - (x,y) - (целое, буква)
     if (x.isdigit()):
         if (CheckNumbInCoord(x)):
-            numb = x
+            x_coord = x
         else:
             print('Введена неверная числовая координата')
             f_error = True
 
-        if (CheckWordInCoord(y)):
-            str = y
+        if (CheckWordInCoord(y) != -1):
+            y_coord = str(CheckWordInCoord(y))
         else:
             print('Введена неверная буквенная координата')
             f_error = True
     elif (y.isdigit()):
         if (CheckNumbInCoord(y)):
-            numb = y
+            x_coord = y
         else:
             print('Введена неверная числовая координата')
             f_error = True
 
-        if (CheckWordInCoord(x)):
-            str = x
+        if (CheckWordInCoord(x) != -1):
+            y_coord = str(CheckWordInCoord(x))
         else:
             print('Введена неверная буквенная координата')
             f_error = True
@@ -80,11 +105,27 @@ def CoordinateRules(xy):
         print('В вашем ходе нет числовой координаты')
         f_error = True
     if(f_error == False):
-        return str + numb
+        return x_coord + y_coord
     return 0
 
-game = FillStartBoard()
-Move()
-if (isGameOver(game)):
-    print('GameOver')
-else:PrintBoard(game)
+# Функция для хода белых
+def FillBoard_White(x_coord, y_coord):
+    board[x_coord - 1][y_coord - 1] = 'W'
+
+# Функция для хода черных
+def FillBoard_Black(x_coord, y_coord):
+    board[x_coord - 1][y_coord - 1] = 'B'
+
+# Основная функция партии
+def GamePlay():
+    f_exit = False
+    while (f_exit == False):
+        f_exit = isGameOver(board)
+        PrintBoard(board)
+        if (Move() == False):
+            f_exit = True
+            print('Игрок завершил партию')
+    else:
+        print('GameOver')
+
+GamePlay()
